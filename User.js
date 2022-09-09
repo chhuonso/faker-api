58285b3132cd19eacb1c95b99c13761c1eea8737
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-09-08 20:36:30 -0700

    User

---
+++ User.js
@@ -0,0 +1,20 @@
+const { faker } = require('@faker-js/faker');
+// remember to use import and NOT require
+// we can create a function to return a random / fake 
+const User= () => {
+    const newUser = {
+        password : faker.internet.password(),
+        email : faker.internet.email(),
+        phoneNumber : faker.phone.number(),
+        lastName : faker.name.lastName(),
+        firstName : faker.name.firstName(),
+        id : faker.datatype.number()
+    };
+    return newUser;
+};
+    
+// const newFakeUser = User();
+// console.log(newFakeUser);
+
+console.log(User())
+module.exports =  User;
