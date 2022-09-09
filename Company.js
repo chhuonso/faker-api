6d209549a6c8f81b2e48b57c46f61b196253b1fe
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-09-08 20:36:06 -0700

    comapy

---
+++ Company.js
@@ -0,0 +1,21 @@
+const { faker } =  require('@faker-js/faker');
+
+const  Company = () => {
+    const newCompany =  {
+        id : faker.datatype.number(),
+        name :faker.company.name(),
+        address :{
+            street : faker.address.streetAddress(),
+            state : faker.address.state(),
+            zipCode : faker.address.zipCode(),
+            country : faker.address.country()
+        }
+    }
+    return newCompany;
+}
+
+// const newFakeCompany = Company();
+// console.log(newFakeCompany);
+
+// console.log(Company())
+module.exports =  Company
